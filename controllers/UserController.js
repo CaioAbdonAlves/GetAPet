f7c950504be8dc80b5076882ec0f556acdf0a880
Caio Abdon <caio.alves54@etec.sp.gov.br>
2023-11-11 15:41:54 -0300

    feat: create controller for get user by id

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -117,4 +117,17 @@ module.exports = class UserController {
 
         res.status(200).json(currentUser);
     }
+
+    static async getUserById(req, res) {
+        const { id } = req.params;
+
+        const user = await User.findById(id).select("-password");
+
+        if(!user) {
+            res.status(422).json({message: "Usuário não encontrado!"});
+            return;
+        }
+
+        res.status(200).json(user);
+    } 
 }
