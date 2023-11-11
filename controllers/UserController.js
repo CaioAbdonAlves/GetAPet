96b5138553f6350cbdea997eab184e8f137ebd66
Caio Abdon <caio.alves54@etec.sp.gov.br>
2023-11-11 15:26:58 -0300

    feat: create methods login and checkuser

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,9 @@
 const User = require("../models/User.js");
 const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+
+const createUserToken = require("../helpers/create-user-token.js");
+const getToken = require("../helpers/get-token.js");
 
 module.exports = class UserController {
     static async register(req, res) {
@@ -42,17 +46,75 @@ module.exports = class UserController {
             return;
         }
 
-        const salt = bcrypt.genSaltSync(10);
+        const salt = bcrypt.genSaltSync(12);
         const hashedPassword = bcrypt.hashSync(password, salt);
 
         const user = new User({name, email, password: hashedPassword, phone});
 
         try {
-            await user.save();
-            res.status(201).json({message: "Usuário criado com sucesso!"});
+            const newUser = await user.save();
+
+            await createUserToken(req, res, newUser);
+
             console.log("Usuário criado com sucesso!");
         }catch(error) {
+            res.status(500).json({message: error});
             console.log(`Ocorreu um erro ao cadastrar o usuário: ${error}`);
         }
+    }
+
+    static async login(req, res) {
+        const { email, password } = req.body;
+
+        if(!email) {
+            res.status(422).json({message: "O e-mail é obrigatório"});
+            return;
+        }
+
+        if(!password) {
+            res.status(422).json({message: "A senha é obrigatória"});
+            return;
+        }
+
+        const userExist = await User.findOne({email: email});
+
+        if(!userExist) {
+            res.status(422).json({message: "O usuário informado não existe!"});
+            return;
+        }
+
+        const passwordMatch = bcrypt.compareSync(password, userExist.password);
+
+        if(!passwordMatch) {
+            res.status(422).json({message: "Senha incorreta!"});
+            return;
+        }
+
+        try {
+            await createUserToken(req, res, userExist);
+            console.log("Usuário logado com sucesso!");
+        }catch(error) {
+            res.status(500).json({message: error});
+            console.log(`Ocorreu um erro ao fazer o login: ${error}`);
+        }
+    }
+
+    static async checkUser(req, res) {
+        let currentUser;
+
+        if(req.headers.authorization) {
+
+            const token = getToken(req);
+            const decoded = jwt.verify(token, 'nossosecret');
+
+            currentUser = await User.findById(decoded.id);
+
+            currentUser.password = undefined;
+
+        } else {
+            currentUser = null;
+        }
+
+        res.status(200).json(currentUser);
     }
 }
