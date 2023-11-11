c987a5cd4c4b1e3cf8f0c1b32b158b52e5695256
Caio Abdon <caio.alves54@etec.sp.gov.br>
2023-11-11 15:26:04 -0300

    feat: add routes login and checkUser

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const UserController = require("../controllers/UserController.js");
 
 router.post('/register', UserController.register);
+router.post('/login', UserController.login);
+router.get('/checkuser', UserController.checkUser);
 
 module.exports = router;
