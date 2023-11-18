0aebea6943d63b9917e514f1b4e31f02b881e2b4
Caio Abdon <caio.alves54@etec.sp.gov.br>
2023-11-18 00:22:16 -0300

    feat: add methods to controller

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 
 const createUserToken = require("../helpers/create-user-token.js");
 const getToken = require("../helpers/get-token.js");
+const getUserByToken = require("../helpers/get-user-by-token.js");
 
 module.exports = class UserController {
     static async register(req, res) {
@@ -129,5 +130,69 @@ module.exports = class UserController {
         }
 
         res.status(200).json(user);
-    } 
+    }
+
+    static async editUser(req, res) {
+        
+        const token = getToken(req);
+        const user = await getUserByToken(token);
+
+        const { name, email, phone, password, confirmpassword } = req.body;
+
+        let image = '';
+
+        if(req.file) {
+            user.image = req.file.filename;
+        }
+
+        if(!name) {
+            res.status(422).json({message: "O nome é obrigatório."});
+            return;
+        }
+
+        user.name = name;
+
+        if(!email) {
+            res.status(422).json({message: "O e-mail é obrigatório."});
+            return;
+        }
+
+        const userExists = await User.findOne({email: email});
+
+        if(user.email !== email && userExists) {
+            res.status(422).json({message: "Este e-mail já está em uso!"});
+            return;
+        }
+
+        user.email = email;
+
+        if(!phone) {
+            res.status(422).json({message: "O telefone é obrigatório."});
+            return;
+        }
+
+        user.phone = phone;
+
+        if(password !== confirmpassword) {
+            res.status(422).json({message: "As senhas não coincidem"});
+            return;
+        } else if(password === confirmpassword && password !== null && password !== undefined) {
+            const salt = bcrypt.genSaltSync(12);
+            const newPassword = bcrypt.hashSync(password, salt);
+
+            user.password = newPassword;
+        }
+
+        try {
+
+            await User.findOneAndUpdate({_id: user._id}, {$set: user}, {new: true});
+
+            res.status(200).json({message: "Usuário atualizado com sucesso!"});
+
+        } catch(err) {
+            res.status(500).json({message: error});
+            return;
+        }
+        
+    }
 }
