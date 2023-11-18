fea88fa2bfd2a2d7de407b3ad674061d3046138d
Caio Abdon <caio.alves54@etec.sp.gov.br>
2023-11-18 00:22:35 -0300

    feat: create routes to user

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -3,9 +3,14 @@ const router = express.Router();
 
 const UserController = require("../controllers/UserController.js");
 
+// middlewares
+const verifyToken = require("../helpers/verify-token.js");
+const { imageUpload } = require("../helpers/image-upload");
+
 router.post('/register', UserController.register);
 router.post('/login', UserController.login);
 router.get('/checkuser', UserController.checkUser);
 router.get('/:id', UserController.getUserById);
+router.patch('/edit/:id', verifyToken, imageUpload.single("image"), UserController.editUser);
 
 module.exports = router;
